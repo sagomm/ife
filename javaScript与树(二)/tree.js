4cbc0e62a8506e1bbe9fb6b2206f478d1fba0884
Administrator <348371873@qq.com>
2016-03-24 10:19:20 +0800

    写了一个树的结构

--- javaScript与树(二)/tree.js
+++ javaScript与树(二)/tree.js
@@ -1,11 +1,19 @@
 function Node(value){
-    this.value = value || NULL;
-    this.child = [];
-         
+    this.value = value || NULL;         
 }
 
 
 function Tree(){
+    this.tree = [];
+}
+Tree.prototype.constructor = 'Tree';
+Tree.prototype.addNode = function (parent,node,callback) {
+    
+}
+Tree.prototype.findNode = function (node) {
+    
+}
+Tree.prototype.deleteTree = function (root) {
     
 }
 
