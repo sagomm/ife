637a2be9abc132374d4982d291f4d3b347cd9e6f
Administrator <348371873@qq.com>
2016-03-24 14:19:11 +0800

    3.24

--- javaScript与树(二)/tree.js
+++ javaScript与树(二)/tree.js
@@ -1,14 +1,38 @@
-function Node(value){
-    this.value = value || NULL;         
+function Node(value,parent){
+    this.value = value || NULL;
+    this.parent = parent || NULL;        
+    this.child = [];  
+}
+Node.prototype.constructor = 'Node';
+Node.prototype.addChild = function (node) {
+    if(node && !(child.indexOf(node))){
+        this.child.push(node);
+    }else{
+        return false;
+    }
+}
+Node.prototype.findChild = function (child){
+    return this.child.indexOf(child) || false;
+}
+Node.prototype.getParent = function () {
+    return this.parent;
+}
+Node.prototype.getChilds = function(){
+    return this.child;
 }
 
 
 function Tree(){
-    this.tree = [];
+    this.root;
+    this.total;
 }
 Tree.prototype.constructor = 'Tree';
-Tree.prototype.addNode = function (parent,node,callback) {
-    
+Tree.prototype.addNode = function (node,parent){
+    if(parent && node){
+        return parent.addChild(node);
+    }else{
+        return false;
+    }
 }
 Tree.prototype.findNode = function (node) {
     
@@ -16,4 +40,12 @@ Tree.prototype.findNode = function (node) {
 Tree.prototype.deleteTree = function (root) {
     
 }
-
+Tree.prototype.DFT = function () {
+    return [];
+}
+Tree.prototype.BFT = function () {
+    return [];
+}
+Tree.prototype.getRoot = function () {
+    return this.root;
+}
