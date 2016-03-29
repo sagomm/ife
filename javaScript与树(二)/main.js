0ba11955d0be31111338c66b071bc1b5874ae346
wangtao <348371873@qq.com>
2016-03-29 16:17:48 +0800

    3.29

--- javaScript与树(二)/main.js
+++ javaScript与树(二)/main.js
@@ -33,18 +33,15 @@ app.Controller.prototype = {
       this.value = value;
     }
     var root = document.getElementById('root');
-    (function getNodes(root){
-      var TreeNode = new Node(new domNode(root,root.getElementsByTagName('span')[0].innerHTML));
-      this.model.addNode(TreeNode);
+    this.model.setRoot(new Node(new domNode(root,root.getElementsByTagName('span')[0].innerHTML));
+    (function getNodes(_root){
+      var TreeNode = new Node(new domNode(_root,root.getElementsByTagName('span')[0].innerHTML));
       for (var i = 0;i< root.childNodes.length;i++){
         if(root.childNodes[i].nodeName === 'DIV'){
-
-        }
+          this.model.addNode(new Node(new domNode(root.clildNodes[i],root.clildNodes[i].getElementsByTagName('span')[0].innerHTML)),_root);
+          getNodes(root.childNodesp[i]);
       }
-
     }).bind(this)(root);
-
-
     document.getElementById('BST').onclick = function() {
       this.model.bst();
     };
@@ -52,7 +49,7 @@ app.Controller.prototype = {
       this.model.dst();
     };
     document.getElementById('search').onclick = function () {
-
+      this.model.search(document.getElementById('text').value);
     };
 
   }
@@ -65,26 +62,25 @@ app.Model = function (view){
 app.Model.prototype = {
   constructor : 'app.Model',
   setRoot : function(root){
-    this.root = root;
+    this.tree.setRoot(root);
   },
   addNode : function(node,parent){
-
+    this.tree.addNode(node,parent);
   },
   bst : function(){
-
+    this.tree.BF(function(node){
+      node.node.addAni();
+    });
   },
   dst : function(){
-
+    this.tree.DF(function(node)){
+      node.node.removeAni();
+    }
   },
   search : function(root){
-
+    this
   }
 }
-app.domNode = function (node,value) {
-    this.node = node;
-    this.value = value;
-}
-
 app.View = function (model) {
   this.model = model;
 
--- javaScript与树(二)/tree.js
+++ javaScript与树(二)/tree.js
@@ -1,7 +1,7 @@
 function Node(value,parent){
     this.value = value || null;
-    this.parent = parent || null;        
-    this.child = [];  
+    this.parent = parent || null;
+    this.child = [];
 }
 Node.prototype.constructor = 'Node';
 Node.prototype.addChild = function (node) {
@@ -23,16 +23,16 @@ Node.prototype.getChilds = function(){
 
 
 function Tree(node){
-    this.root = node || null;
+    this.root = node;
     this.total = 0;
 }
 Tree.prototype.constructor = 'Tree';
-Tree.prototype.findNode = function (node) {
+Tree.prototype.findNode = function (node,callback) {
     var res = false;
     this.BF(function(_node){
         if(node === _node){
-           res =  true;         
-        }    
+           res =  true;
+        }
     });
     return res;
 }
@@ -55,34 +55,37 @@ Tree.prototype.deleteNode = function (node,parent) {
             parent.child.splice(_index,1);
         }else{
             return false;
-        } 
+        }
     }else{
         return false;
-    }             
+    }
 }
 Tree.prototype.DF = function (callback) {
     (function _DF(root){
         if(root){
             for(var i = 0 ;i < root.child.length;i++){
-                _DF(root.child[i]); 
+                _DF(root.child[i]);
             }
-            callback(root);                        
+            callback(root);
         }
     })(this.root);
 }
 Tree.prototype.BF = function (callback){
     var _queue = [];
     if(this.root){
-        _queue.push(this.root);   
+        _queue.push(this.root);
         while(_queue.length){
             var _node = _queue.pop();
             for(var i = 0;i<_node.child.length;i++){
                 _queue.push(_node.child[i]);
             }
-            callback(_node);    
-        }  
-    };        
+            callback(_node);
+        }
+    };
 }
 Tree.prototype.getRoot = function () {
-    return this.root || false;
+    return this.root;
+}
+Tree.prototype.setRoot = function (root) {
+    this.root = root;
 }
