a6302f9a40f73ab92885f48b528f74a14d0da53a
sagomm <348371873@qq.com>
2016-04-11 16:36:45 +0800

    4.11

---
+++ 听指令的小方块/js/init.js
@@ -0,0 +1,31 @@
+var page = {}
+
+/**
+ * 绘制背景
+ */
+page.drowBackground = function(canvas){
+  var ctx = canvas.getContext("2d");
+  ctx.strokeStyle = '#ccc';
+  ctx.beginPath();
+  for(var i = 1 ;i < 10 ;i++){
+    /**
+     * 绘制纵向网格线
+     */
+    ctx.moveTo(i*50,0);
+    console.log(1);
+    ctx.lineTo(i*50,500);
+    /**
+     * 绘制横向网格线
+     */
+     ctx.moveTo(0,i*50);
+     ctx.lineTo(500,i*50);
+     ctx.stroke();
+  }
+  ctx.closePath();
+}
+/**
+ * 画小方块
+ */
+page.box = function(canvas,x,y){
+
+}
